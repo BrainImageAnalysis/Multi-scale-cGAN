f2fb193bf43eefa29a8a51fdc671d2de77efa81b
megdec <meghane.decroocq@protonmail.com>
2024-07-01 10:15:22 +0900

    add README

---
+++ README.md
@@ -0,0 +1,24 @@
+# Multi-scale Stochastic Generation of Labelled Microscopy Images using a cGAN Cascade
+
+This is the code of the model proposed in the "Multi-scale Stochastic Generation of Labelled Microscopy
+Images for Neuron Segmentation" paper accepted for the MIDL 2024 conference. <https://openreview.net/forum?id=orE18Wdgbj>
+
+## Overview 
+
+The objective of this model is to automatically generate realistic microscopy images, conditioned on a given neuron morphology input. A multi-scale cascade of conditional generative adversarial networks iteratively refines the generated image resolution using patches of decreasing field-of-view. The diversity of the generated images is enhanced through the loss function and random vectors.
+
+<img src="simplified_scales.png" alt="Concept of the multi-scale image generation." width="1000"/>
+
+The labelled synthetic image generated can be used to train tracing models for instance. The method can be extended to other tree-like structures such as vessels (e.g. generation of synthetic retina images). 
+
+
+## How to use
+
+1) Prepare your dataset by organizing images in two separate folders, one for the target images (e.g. microscopy images) and one for the labels (e.g. neuron segmentation). More than one type of labels can be used (e.g. veins / arteries).
+
+2) Follow the steps described in the main file. 
+
+## References
+
+If you use this code, please cite the following article:
+
--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 from MultiResGenerator import MultiResGenerator
 
 
-img_folder = "path_to_img_folder"
-gt_folder = "path_to_label_folder"
-output_folder = "path_to_output_folder"
+img_folder = "path_to_img_folder/"
+gt_folder = "path_to_label_folder/"
+output_folder = "path_to_output_folder/"
+
 
 # Patch-related hyperparameters
 number_of_scales = 5
 real_patch_size = [[1024, 1024], [512, 512], [256,256] , [128,128], [128,128]] # Size of the field of view to consider for each scale
 model_patch_size = [[64, 64], [64, 64], [64, 64], [64,64], [128,128]] # Size of the patch (model input) for each scale
 stride = [0, 256, 128, 64, 64] # Stride of the field of view used to draw patches 
-
 augmentation = [["flip_vertical", "flip_horizontal", "rotation90", "rotation180"], ["none"], ["none"], ["none"], ["none"]] # Augmentation to apply for each scale
 model_type = ["ModeSeekingcGAN", "RefinementcGAN", "RefinementcGAN", "RefinementcGAN", "RefinementcGAN"] # Type of model to use for each scale
 
 # Other hyperparameters
 lbd = [1, 10, 10, 10, 10] # Loss balance parameter lbd (depends on the type of model)
-itrs = [3000, 3000, 6000, 6000, 6000] # Number of iteration to run for each scale
-lr = [0.0002, 0.0002, 0.0002, 0.0002, 0.0002] # Learning rate to use for each scale
+itrs = [6000, 6000, 6000, 6000, 6000] # Number of iteration to run for each scale
+lr = [0.0001, 0.0001, 0.0001, 0.0001, 0.0001] # Learning rate to use for each scale
 batch_size = [32, 32, 32, 32, 32] # Batch size for each scale
 nz = 12 # Size of the random vector used for ModeSeekingcGAN
+
 
 
 ### TRAINING ALL ###
@@ -28,8 +29,8 @@ nz = 12 # Size of the random vector used for ModeSeekingcGAN
 model = MultiResGenerator(output_folder, len(real_patch_size), real_patch_size, model_patch_size, model_type, itrs, augmentation, stride, lr, lbd, nz, batch_size)
 # Load the images and labels. In our experience, to generate non histogram equalized images, it is crucial to provide both the equalized and non-equalized version of the original images.
 # The images and labels must have the same file names. Optional : file_list argument to load only part of the images contained in folder (given by a list of file names)
-model.load_images([train_img_folder, train_img_folder, train_gt_folder], data_type = ["img","img","gt"], dataset_name = "train", equalize = [True, False, False], data_range = [-1,1])
-model.run_training_all() # Train all scales one-by-one
+model.load_images([img_folder, img_folder, gt_folder], data_type = ["img","img", "gt"], dataset_name = "train", equalize = [True, False, False], data_range = [-1,1])
+model.run_training_all("train") # Train all scales one-by-one
 
 model.write_images(number_of_scales-1, "train", "gen_" + str(number_of_scales-1)) # Write images generated at the last scales
 
@@ -39,7 +40,7 @@ model.write_images(number_of_scales-1, "train", "gen_" + str(number_of_scales-1)
 # Create the cascade model
 model = MultiResGenerator(output_folder, len(real_patch_size), real_patch_size, model_patch_size, model_type, itrs, augmentation, stride, lr, lbd, nz, batch_size)
 # Load the images and labels
-model.load_images([train_img_folder, train_img_folder, train_gt_folder], data_type = ["img","img","gt"], dataset_name = "train", equalize = [True, False, False], data_range = [-1,1])
+model.load_images([img_folder, img_folder, gt_folder], data_type = ["img","img","gt"], dataset_name = "train", equalize = [True, False, False], data_range = [-1,1])
 # Load all the previously trained models
 model.load_state_dicts()
 
@@ -53,20 +54,22 @@ for i in range(start_scale, number_of_scales):
 model.write_images(number_of_scales-1, "train", "gen_" + str(number_of_scales-1)) # Write images generated at the last scales
 
 
+
 ### EVALUATION ###
+test_gt_folder = "path_to_test_label_folder"
+
 
 # Load the full cascade weights 
 augmentation = [["none"], ["none"], ["none"], ["none"], ["none"]]
-model = MultiResGenerator(output_folder, len(real_patch_size), real_patch_size, model_patch_size, model_type, itrs, augmentation, stride, lr, lbd, nz, batch_size)
+model = MultiResGenerator(output_folder, len(real_patch_size), real_patch_size, model_patch_size, model_type, itrs, augmentation, stride, lr, lbd, nz, batch_size,  n_img_channels = 2)
 
-model.load_images([train_gt_folder], ["gt"], "test", [False], data_range = [-1,1])
+model.load_images([test_gt_folder], ["gt"], "test", [False], data_range = [-1,1])
 model.load_state_dicts()
 
 for i in range(len(itrs)):
 	model.run_evaluation(i, "test", write = False)
 
 model.write_images(number_of_scales-1, "test", "gen_" + str(number_of_scales-1))
-
 """
 
 
